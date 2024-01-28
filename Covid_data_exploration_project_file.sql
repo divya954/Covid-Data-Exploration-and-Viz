ff1ca505149f42b85538a4fbb9fa92c73b688b15
divya954 <64891765+divya954@users.noreply.github.com>
2024-01-28 17:07:46 +1100

    Add files via upload

--- Covid_data_exploration_project_file.sql
+++ Covid_data_exploration_project_file.sql
@@ -1,3 +1,8 @@
+
+/*
+Covid data exploration PortfolioProject
+*/
+
 select * from PortfolioProject..CovidDeaths
 order by 3,4
 
@@ -41,7 +46,7 @@ where continent is not null
 order by 1,2
 
 
--- shows what percentage of population got covid in your country
+-- shows what percentage of population got covid across different country on day to day basis
 
 select location, date, total_cases, population, convert(float, total_cases)/convert(float, population) * 100 as new_case_percent
 from PortfolioProject..CovidDeaths
@@ -49,6 +54,14 @@ from PortfolioProject..CovidDeaths
 where continent is not null
 order by 1,2
 
+-- shows what percentage of population got covid in your country
+
+
+select location, date, total_cases, population, convert(float, total_cases)/convert(float, population) * 100 as new_case_percent
+from PortfolioProject..CovidDeaths
+where location like '%australia%'
+and continent is not null
+order by 1,2
 
 -- looking at countries with highest infection rate compared to population
 
@@ -67,7 +80,7 @@ from PortfolioProject..CovidDeaths
 group by location
 order by TotalDeathCount desc;
 
--- showing the countries with the highest death count per population with the filter not including the continents with null value
+-- showing the countries with the highest death count per population with the filter (not including the continents with null value)
 
 select location,  Max(convert (float, total_deaths)) as TotalDeathCount
 from PortfolioProject..CovidDeaths
@@ -83,6 +96,7 @@ group by continent
 having continent is not null
 order by TotalDeathCount desc;
 
+-- following query gives the same result as above
 
 select continent,  Max(convert (float, total_deaths)) as TotalDeathCount
 from PortfolioProject..CovidDeaths
@@ -91,9 +105,9 @@ group by continent
 --having continent is not null
 order by TotalDeathCount desc;
 
--- the above one is not giving the right calculation for total death in north america so we are doing it again 
--- a different way
-
+/*the above one is not giving the right calculation for total death in north america so 
+we are doing it again  a different way
+*/
 
 select location,  Max(convert (float, total_deaths)) as TotalDeathCount
 from PortfolioProject..CovidDeaths
@@ -101,7 +115,7 @@ where continent is  null
 group by location
 order by TotalDeathCount desc;
 
--- you can go back up and remove the continent is not null if you want but  not necessarily
+-- we can go back up and remove the continent is not null for more accurate result if we want but not necessarily
 
 
 
@@ -114,37 +128,43 @@ where continent is not null
 group by continent
 order by DeathPopulationPercent desc;
 
--- GLOBAL NUMBERS (we want to calculate everything for the across the entire world)
+-- GLOBAL NUMBERS (we want to calculate everything across the entire world)
 
 -- calculating death per population percentage accross the world on each date from start to end
 
--- the below query only calculate the death percentage
--- there are same dates with multiple total_deaths, this query pic the max total_deaths amoung those same dates and do the calculation, which isn't what we meant to do, 
--- we meant to find the total cases that happend in that particular death which can be achieved only if we add all the new cases around the world on that specific date. 
--- look at the below's 3rd query for the correct answer
-
-select date, Max(total_cases) as max_cases, Max(cast (total_deaths as int)) as max_deaths,   Max(cast (total_deaths as int)) / Max(total_cases) * 100 as DeathPerPopulationPercentage
+/*
+ Below query only calculates the death percentage. There are same dates with multiple total_deaths based on different locations/countries , 
+ this query picks the max total_deaths amoung those same dates and do the calculation, which isn't what we want to do. the results won't be accurate
+ we want to find the total cases that happened in that particular date regardless of locations, which can only be achieved 
+ if we add all the new cases around the world on that specific date. 
+ look at the below's 3rd query for the correct answer
+*/
+select date, Max(total_cases) as max_cases, Max(cast (total_deaths as int)) as max_deaths,   
+Max(cast (total_deaths as int)) / Max(total_cases) * 100 as DeathPerPopulationPercentage
 from PortfolioProject..CovidDeaths
 group by date
 order by date ;
 
 
-select  Max(total_cases) as max_cases, Max(cast (total_deaths as int)) as max_deaths,   Max(cast (total_deaths as int)) / Max(total_cases) * 100 as DeathPerPopulationPercentage
+select  Max(total_cases) as max_cases, Max(cast (total_deaths as int)) as max_deaths,  
+Max(cast (total_deaths as int)) / Max(total_cases) * 100 as DeathPerPopulationPercentage
 from PortfolioProject..CovidDeaths ;
 
--- this calculate the total cases and total deaths occured on that specific date accross the world irrespective of the location and then calculate the deaths per cases on that specific death accross the world,
--- thats why we can't just take the max total_caes and max total_death to calculate the death on a specific day we use those only when we are doing calculation for the specific location. 
+/*
+ The below query calculates the total cases and total deaths that occurred on a specific date accross the world, irrespective of the location.
+ Then, it calculates the deaths per case on that specific date worldwide. That's why, we can't just take the max total_cases and max total_death 
+ to calculate the death per case on a specific day, we use those only when we are doing calculation for death per case for a specific location. 
+*/
 
 
-
-select date, sum(new_cases) as total_cases, sum(cast (new_deaths as int)) as total_deaths,   sum(cast (new_deaths as int)) / sum(new_cases) * 100 as DeathPerPopulationPercentage
+select date, sum(new_cases) as total_cases, sum(cast (new_deaths as int)) as total_deaths,   sum(cast (new_deaths as int)) / sum(new_cases) * 100 as DeathPerCasePercentage
 from PortfolioProject..CovidDeaths
 where continent is not null
 group by date
 order by date;
 
 
--- overall across the world the total deaths per percentage irrespective of death or any specific locatin
+-- shows the total deaths per case percentage irrespective of date or locatin worldwide.
 
 select  sum(new_cases) as total_cases, sum(cast (new_deaths as int)) as total_deaths,   sum(cast (new_deaths as int)) / sum(new_cases) * 100 as DeathPerPopulationPercentage
 from PortfolioProject..CovidDeaths
@@ -164,9 +184,12 @@ on dea.location = vac.location  and dea.date = vac.date
 where dea.continent is not null
 order by 2,3;
 
+/*
+looking at the total vaccination with use of new vaccinations data
+*/
 
--- looking at the total vaccinatin with use of new vaccinatin data
--- looking at total population vs vaccinations
+
+-- looking at total population vs vaccinations accross different continents
 
 
 select dea.continent, dea.location, dea.date, dea.population, sum(cast (vac.new_vaccinations as int)) over (partition by dea.continent) as total_vaccination_per_continent
@@ -176,6 +199,7 @@ on dea.location = vac.location  and dea.date = vac.date
 where dea.continent is not null
 order by 2,3;
 
+-- looking at total population vs vaccinations accross different locations/countries
 
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 sum(cast (vac.new_vaccinations as int)) over (partition by dea.location) as total_vaccination_per_location
@@ -185,8 +209,12 @@ on dea.location = vac.location  and dea.date = vac.date
 where dea.continent is not null
 order by 2,3;
 
--- order by location and date within the partition by in the below query to see the real time addtion of total vaccination as the new_vacination keep going up
--- I haven't been able to understand how it worked
+/*
+use of order by location and date within the partition by clause in the below query to see the real time addtion of total vaccination 
+as the new_vacination keep going up
+*/
+
+
 
 
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
@@ -198,7 +226,7 @@ where dea.continent is not null
 order by 2,3;
 
 
--- more practice myself - below is the changes made by the above query made with the use of order by with in partition by clause
+-- below is the changes made by the above query with the use of "order by" within  the "partition by" clause
 
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 sum(cast (vac.new_vaccinations as int)) over (partition by dea.location Order by dea.location, dea.date) as RollingPeopleVaccinated
@@ -209,15 +237,10 @@ where dea.continent is not null
 and dea.location like '%albania%' and vac.new_vaccinations is not null
 order by 2,3;
 
---  i think i under stnad this a bit  (look at the part 3 data exploration note)
--- 59:00
-
--- 
+ 
 
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 sum(cast (vac.new_vaccinations as int)) over (partition by dea.location Order by dea.location, dea.date) as RollingPeopleVaccinated
--- ,(RollingPeopleVaccinated / population) * 100  -- to know how many people in that country is vaccinated
- -- we can't use a column that you just created to use as the next one, so what we need to do is create either a CTE or a temp table.
 from PortfolioProject..CovidDeaths as dea
 Join PortfolioProject..CovidVaccinations as vac
 on dea.location = vac.location  and dea.date = vac.date
@@ -225,9 +248,9 @@ where dea.continent is not null
 order by 2,3;
 
 
--- you can either creat a cte or temp table
 
--- USE CTE
+
+-- USE Common Table Expression (CTE)
 
 with PopvsVac (continent, Location, Date, population, new_vaccinations, RollingPeopleVaccinated)
 as (  
@@ -241,12 +264,10 @@ where dea.continent is not null
 --order by 2,3
 
 )
-
-select *, (RollingPeopleVaccinated/population) * 100  
+select *, (RollingPeopleVaccinated/population) * 100   as RollingPeopleVaccinated_per_population
 from PopvsVac
 
-
---select *, (RollingPeopleVaccinated/population) * 100  as percentageV
+--select *, (RollingPeopleVaccinated/population) * 100  as RollingPeopleVaccinated_per_population
 --from PopvsVac
 --where location like '%albania%'  and new_vaccinations is not null;
 
@@ -275,15 +296,15 @@ on dea.location = vac.location  and dea.date = vac.date
 order by 2,3;
 
 
-select *, (RollingPeopleVaccinated/population) * 100  
+select *, (RollingPeopleVaccinated/population) * 100 as RollingPeopleVaccinated_per_population
 from #PercentPopulationVaccinated
 
--- Creating view to store dat a for later visualizations
-
+-- Creating view to store data for later visualizations
 
 create view PercentPopulationVaccinated as
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
-sum(cast (vac.new_vaccinations as int)) over (partition by dea.location Order by dea.location, dea.date) as RollingPeopleVaccinated
+sum(cast (vac.new_vaccinations as int)) over (partition by dea.location Order by dea.location, dea.date)
+as RollingPeopleVaccinated
 from PortfolioProject..CovidDeaths as dea
 Join PortfolioProject..CovidVaccinations as vac
 on dea.location = vac.location  and dea.date = vac.date
@@ -292,16 +313,7 @@ where dea.continent is not null
 
 
 
-
-
-
-
---select continent,  Max(convert (float, total_deaths)) as TotalDeathCount
---from PortfolioProject..CovidDeaths
---where continent is not null
---group by continent
-----having continent is not null
---order by TotalDeathCount desc;
+--------------------------------------------The End-----------------------------------------------
 
 
 
